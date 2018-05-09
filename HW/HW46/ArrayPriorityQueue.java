0bcb37019bb80633430b07771a2903d02c4894b3
Andrew Shao <ashao1@stuy.edu>
2018-05-09 18:20:43 -0400

    Finished HW46 solution

--- HW/HW46/ArrayPriorityQueue.java
+++ HW/HW46/ArrayPriorityQueue.java
@@ -1,41 +1,70 @@
-import java.util.PriorityQueue;
+//	Andrew Shao
+//	APCS2 pd2
+//	HW46 -- Arrr, There Be Priorities Here Matey
+//	2018-05-10R
+
 import java.util.ArrayList;
 
 public class ArrayPriorityQueue implements PriorityQueue
 {
-    private ArrayList _queue;
+	
+    private ArrayList<String> _queue;
 
     public ArrayPriorityQueue()
     {
-	_queue = new ArrayList();
+	_queue = new ArrayList<String>();
     }
 
-    public void add( Object o )
+	// Runtime: O(1)
+    public void add( String o )
     {
 	_queue.add( o );
     }
 
-    public Object removeMin()
+	// Runtime: O(n)
+    public String removeMin()
     {
 	int index = 0;
 	for( int i = 0; i < _queue.size(); i++ )
-	    if ( ((String)_queue.get( i )).compareTo( (String)_queue.get( index ) ) < 0 )
+	    if ( _queue.get( i ).compareTo( _queue.get( index ) ) < 0 )
 		index = i;
 	return _queue.remove( index );
     }
 
-    public Object peekMin()
+	// Runtime: O(n)
+    public String peekMin()
     {
 	int index = 0;
 	for( int i = 0; i < _queue.size(); i++ )
-	    if ( ((String)_queue.get( i )).compareTo( (String)_queue.get( index ) ) < 0 )
+	    if ( _queue.get( i ).compareTo( _queue.get( index ) ) < 0 )
 		index = i;
 	return _queue.get( index );
     }
 
+	// Runtime: O(1)
     public boolean isEmpty()
     {
 	return _queue.isEmpty();
     }
+	
+	public static void main( String args[] )
+	{
+		ArrayPriorityQueue apq = new ArrayPriorityQueue();
+		
+		apq.add( "nunu" );
+		apq.add( "challenged" );
+		apq.add( "bronze" );
+		apq.add( "challenger" );
+		apq.add( "smite" );
+		apq.add( "consume" ); 
+		apq.add( "buff" );
+		apq.add( "jungle" );		
+		// bronze buff challenged challenger consume jungle nunu smite
+		
+		System.out.println( apq.peekMin() ); // bronze
+		
+		while( !apq.isEmpty() )
+			System.out.print( apq.removeMin() + " " );			
+	}
 	
 }
---
+++ HW/HW46/PriorityQueue.java
@@ -0,0 +1,17 @@
+//	Andrew Shao
+//	APCS2 pd2
+//	HW46 -- Arrr, There Be Priorities Here Matey
+//	2018-05-10R
+
+public interface PriorityQueue
+{
+	
+	public void add( String x );
+	
+	public boolean isEmpty();
+	
+	public String peekMin();
+	
+	public String removeMin();
+	
+}
